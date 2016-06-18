9be603292b644c45dc97bf726a0c1a3ae42849fc
Kayan Silva <kayanf.silva@gmail.com>
2016-06-18 04:20:54 -0300

    Modificações no codigo de seleção.

--- PrjForumDistriCSharp/Form1.Designer.cs
+++ PrjForumDistriCSharp/Form1.Designer.cs
@@ -28,81 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnPesquisar = new System.Windows.Forms.Button();
-            this.rdbCivel = new System.Windows.Forms.RadioButton();
-            this.rdbCriminal = new System.Windows.Forms.RadioButton();
             this.txtnome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblcheck = new System.Windows.Forms.Label();
             this.btnlimpar = new System.Windows.Forms.Button();
             this.btnsair = new System.Windows.Forms.Button();
             this.datagridconsulta = new System.Windows.Forms.DataGridView();
+            this.dtpDistribuicao = new System.Windows.Forms.DateTimePicker();
+            this.rdbcivel = new System.Windows.Forms.RadioButton();
+            this.rdbcriminal = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rdbnome = new System.Windows.Forms.RadioButton();
+            this.rdbdatadistri = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.datagridconsulta)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnPesquisar
             // 
-            this.btnPesquisar.Location = new System.Drawing.Point(15, 93);
+            this.btnPesquisar.Location = new System.Drawing.Point(502, 53);
+            this.btnPesquisar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnPesquisar.Name = "btnPesquisar";
-            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.Size = new System.Drawing.Size(87, 28);
             this.btnPesquisar.TabIndex = 0;
             this.btnPesquisar.Text = "Pesquisar";
             this.btnPesquisar.UseVisualStyleBackColor = true;
             this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
             // 
-            // rdbCivel
-            // 
-            this.rdbCivel.AutoSize = true;
-            this.rdbCivel.Location = new System.Drawing.Point(120, 30);
-            this.rdbCivel.Name = "rdbCivel";
-            this.rdbCivel.Size = new System.Drawing.Size(48, 17);
-            this.rdbCivel.TabIndex = 1;
-            this.rdbCivel.TabStop = true;
-            this.rdbCivel.Text = "Civel";
-            this.rdbCivel.UseVisualStyleBackColor = true;
-            // 
-            // rdbCriminal
-            // 
-            this.rdbCriminal.AutoSize = true;
-            this.rdbCriminal.Location = new System.Drawing.Point(199, 30);
-            this.rdbCriminal.Name = "rdbCriminal";
-            this.rdbCriminal.Size = new System.Drawing.Size(61, 17);
-            this.rdbCriminal.TabIndex = 2;
-            this.rdbCriminal.TabStop = true;
-            this.rdbCriminal.Text = "Criminal";
-            this.rdbCriminal.UseVisualStyleBackColor = true;
-            // 
             // txtnome
             // 
-            this.txtnome.Location = new System.Drawing.Point(93, 56);
+            this.txtnome.Location = new System.Drawing.Point(106, 56);
+            this.txtnome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtnome.Name = "txtnome";
-            this.txtnome.Size = new System.Drawing.Size(313, 20);
-            this.txtnome.TabIndex = 3;
+            this.txtnome.Size = new System.Drawing.Size(364, 22);
+            this.txtnome.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.Size = new System.Drawing.Size(99, 16);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Pesquisa entre:";
+            this.label1.Text = "Pesquise entre:";
             // 
-            // label2
+            // lblcheck
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Digite o nome:";
+            this.lblcheck.AutoSize = true;
+            this.lblcheck.Location = new System.Drawing.Point(8, 59);
+            this.lblcheck.Name = "lblcheck";
+            this.lblcheck.Size = new System.Drawing.Size(92, 16);
+            this.lblcheck.TabIndex = 6;
+            this.lblcheck.Text = "Digite o nome:";
             // 
             // btnlimpar
             // 
-            this.btnlimpar.Location = new System.Drawing.Point(15, 340);
+            this.btnlimpar.Location = new System.Drawing.Point(605, 53);
+            this.btnlimpar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnlimpar.Name = "btnlimpar";
-            this.btnlimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpar.Size = new System.Drawing.Size(87, 28);
             this.btnlimpar.TabIndex = 7;
             this.btnlimpar.Text = "Limpar";
             this.btnlimpar.UseVisualStyleBackColor = true;
@@ -110,9 +96,10 @@
             // 
             // btnsair
             // 
-            this.btnsair.Location = new System.Drawing.Point(822, 348);
+            this.btnsair.Location = new System.Drawing.Point(964, 292);
+            this.btnsair.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnsair.Name = "btnsair";
-            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.Size = new System.Drawing.Size(87, 28);
             this.btnsair.TabIndex = 8;
             this.btnsair.Text = "Sair";
             this.btnsair.UseVisualStyleBackColor = true;
@@ -123,38 +110,109 @@
             this.datagridconsulta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.datagridconsulta.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
             this.datagridconsulta.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.datagridconsulta.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.datagridconsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.datagridconsulta.Enabled = false;
-            this.datagridconsulta.Location = new System.Drawing.Point(20, 131);
+            this.datagridconsulta.Location = new System.Drawing.Point(12, 99);
+            this.datagridconsulta.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.datagridconsulta.Name = "datagridconsulta";
             this.datagridconsulta.RowHeadersVisible = false;
             this.datagridconsulta.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.datagridconsulta.Size = new System.Drawing.Size(891, 150);
+            this.datagridconsulta.Size = new System.Drawing.Size(1039, 185);
             this.datagridconsulta.TabIndex = 9;
+            // 
+            // dtpDistribuicao
+            // 
+            this.dtpDistribuicao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDistribuicao.Location = new System.Drawing.Point(190, 56);
+            this.dtpDistribuicao.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpDistribuicao.Name = "dtpDistribuicao";
+            this.dtpDistribuicao.Size = new System.Drawing.Size(110, 22);
+            this.dtpDistribuicao.TabIndex = 10;
+            // 
+            // rdbcivel
+            // 
+            this.rdbcivel.AutoSize = true;
+            this.rdbcivel.Location = new System.Drawing.Point(3, 7);
+            this.rdbcivel.Name = "rdbcivel";
+            this.rdbcivel.Size = new System.Drawing.Size(53, 20);
+            this.rdbcivel.TabIndex = 11;
+            this.rdbcivel.TabStop = true;
+            this.rdbcivel.Text = "Cível";
+            this.rdbcivel.UseVisualStyleBackColor = true;
+            // 
+            // rdbcriminal
+            // 
+            this.rdbcriminal.AutoSize = true;
+            this.rdbcriminal.Location = new System.Drawing.Point(83, 7);
+            this.rdbcriminal.Name = "rdbcriminal";
+            this.rdbcriminal.Size = new System.Drawing.Size(73, 20);
+            this.rdbcriminal.TabIndex = 12;
+            this.rdbcriminal.TabStop = true;
+            this.rdbcriminal.Text = "Criminal";
+            this.rdbcriminal.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdbcriminal);
+            this.panel1.Controls.Add(this.rdbcivel);
+            this.panel1.Location = new System.Drawing.Point(117, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(159, 37);
+            this.panel1.TabIndex = 14;
+            // 
+            // rdbnome
+            // 
+            this.rdbnome.AutoSize = true;
+            this.rdbnome.Location = new System.Drawing.Point(296, 7);
+            this.rdbnome.Name = "rdbnome";
+            this.rdbnome.Size = new System.Drawing.Size(60, 20);
+            this.rdbnome.TabIndex = 15;
+            this.rdbnome.TabStop = true;
+            this.rdbnome.Text = "Nome";
+            this.rdbnome.UseVisualStyleBackColor = true;
+            this.rdbnome.CheckedChanged += new System.EventHandler(this.rdbnome_CheckedChanged);
+            // 
+            // rdbdatadistri
+            // 
+            this.rdbdatadistri.AutoSize = true;
+            this.rdbdatadistri.Location = new System.Drawing.Point(362, 7);
+            this.rdbdatadistri.Name = "rdbdatadistri";
+            this.rdbdatadistri.Size = new System.Drawing.Size(143, 20);
+            this.rdbdatadistri.TabIndex = 16;
+            this.rdbdatadistri.TabStop = true;
+            this.rdbdatadistri.Text = "Data de Distribuição";
+            this.rdbdatadistri.UseVisualStyleBackColor = true;
+            this.rdbdatadistri.CheckedChanged += new System.EventHandler(this.rdbdatadistri_CheckedChanged);
             // 
             // FrmConsulta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(923, 383);
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(1077, 331);
+            this.Controls.Add(this.rdbdatadistri);
+            this.Controls.Add(this.rdbnome);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpDistribuicao);
             this.Controls.Add(this.datagridconsulta);
             this.Controls.Add(this.btnsair);
             this.Controls.Add(this.btnlimpar);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblcheck);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtnome);
-            this.Controls.Add(this.rdbCriminal);
-            this.Controls.Add(this.rdbCivel);
             this.Controls.Add(this.btnPesquisar);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "FrmConsulta";
             this.ShowIcon = false;
@@ -162,6 +220,8 @@
             this.Text = "KaySoft - Consultas do Distribuidor - Guarujá";
             this.Load += new System.EventHandler(this.FrmConsulta_Load);
             ((System.ComponentModel.ISupportInitialize)(this.datagridconsulta)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,14 +230,19 @@
         #endregion
 
         private System.Windows.Forms.Button btnPesquisar;
-        private System.Windows.Forms.RadioButton rdbCivel;
-        private System.Windows.Forms.RadioButton rdbCriminal;
         private System.Windows.Forms.TextBox txtnome;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblcheck;
         private System.Windows.Forms.Button btnlimpar;
         private System.Windows.Forms.Button btnsair;
         private System.Windows.Forms.DataGridView datagridconsulta;
+        private System.Windows.Forms.DateTimePicker dtpDistribuicao;
+        private System.Windows.Forms.RadioButton rdbcivel;
+        private System.Windows.Forms.RadioButton rdbcriminal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rdbnome;
+        private System.Windows.Forms.RadioButton rdbdatadistri;
     }
 }
 
--- PrjForumDistriCSharp/Form1.cs
+++ PrjForumDistriCSharp/Form1.cs
@@ -14,6 +14,9 @@ namespace PrjForumDistriCSharp
 {
     public partial class FrmConsulta : Form
     {
+        string selectnobanco;
+        string datainvertida;
+
         public FrmConsulta()
         {
             InitializeComponent();
@@ -21,7 +24,11 @@ namespace PrjForumDistriCSharp
 
         private void FrmConsulta_Load(object sender, EventArgs e)
         {
-            rdbCivel.Checked = true;
+            rdbcivel.Checked = true;
+            rdbnome.Checked = true;
+            lblcheck.Text = "Digite o nome:";
+            txtnome.Focus();
+            dtpDistribuicao.Visible = false;
         }
 
         private void btnlimpar_Click(object sender, EventArgs e)
@@ -38,31 +45,86 @@ namespace PrjForumDistriCSharp
         private void btnPesquisar_Click(object sender, EventArgs e)
         {
             string strcon = "Server=tcp:kaysoft.database.windows.net,1433;Data Source=kaysoft.database.windows.net;Initial Catalog=DistribuidorGuaruja;Persist Security Info=False;User ID=kayanfds;Password=Kayas190393;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection conexao = new SqlConnection(strcon);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
 
-            SqlConnection conexao = new SqlConnection(strcon);          
-            SqlCommand cmdsql = new SqlCommand("Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where nm_requerido = '"+txtnome.Text+"'", conexao);           
+            if (rdbcivel.Checked)
+            {
+                if (rdbnome.Checked)
+                {
+                    selectnobanco = "Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where nm_requerido = '" + txtnome.Text + "'";
+                    SqlCommand cmdsql = new SqlCommand(selectnobanco, conexao);
+                    conexao.Open();
+                    cmdsql.ExecuteNonQuery();
+                    da.SelectCommand = cmdsql;
+                    da.Fill(ds);
+                    datagridconsulta.DataSource = ds;
+                    datagridconsulta.DataMember = ds.Tables[0].TableName;
+                    conexao.Close();
+                    if (datagridconsulta.RowCount <= 1)
+                        MessageBox.Show("Nenhum registro foi encontrado.");
+                }
+                else
+                {
+                    selectnobanco = "Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where dt_distribuicao = '"+ dtpDistribuicao.Value.ToString("yyyyMMdd") + "'";
+                    SqlCommand cmdsql = new SqlCommand(selectnobanco, conexao);
+                    conexao.Open();
+                    cmdsql.ExecuteNonQuery();
+                    da.SelectCommand = cmdsql;
+                    da.Fill(ds);
+                    datagridconsulta.DataSource = ds;
+                    datagridconsulta.DataMember = ds.Tables[0].TableName;
+                    conexao.Close();
+                    if (datagridconsulta.RowCount <= 1)
+                        MessageBox.Show("Nenhum registro foi encontrado.");
+                }
+            }
+            else
+            {
+                if (rdbnome.Checked)
+                {
+                    selectnobanco = "Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where nm_requerido = '" + txtnome.Text + "'";
+                    SqlCommand cmdsql = new SqlCommand(selectnobanco, conexao);
+                    conexao.Open();
+                    cmdsql.ExecuteNonQuery();
+                    da.SelectCommand = cmdsql;
+                    da.Fill(ds);
+                    datagridconsulta.DataSource = ds;
+                    datagridconsulta.DataMember = ds.Tables[0].TableName;
+                    conexao.Close();
+                    if (datagridconsulta.RowCount <= 1)
+                        MessageBox.Show("Nenhum registro foi encontrado.");
+                }
+                else
+                {
+                    selectnobanco = "Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where dt_distribuicao = '" + dtpDistribuicao.Value.ToString("yyyyMMdd") + "'";
+                    SqlCommand cmdsql = new SqlCommand(selectnobanco, conexao);
+                    conexao.Open();
+                    cmdsql.ExecuteNonQuery();
+                    da.SelectCommand = cmdsql;
+                    da.Fill(ds);
+                    datagridconsulta.DataSource = ds;
+                    datagridconsulta.DataMember = ds.Tables[0].TableName;
+                    conexao.Close();
+                    if (datagridconsulta.RowCount <= 1)
+                        MessageBox.Show("Nenhum registro foi encontrado.");
+                }
+            }
+        }
 
-            try
-            {
-                conexao.Open();
-                cmdsql.ExecuteNonQuery();
-                SqlDataAdapter da = new SqlDataAdapter();
-                DataSet ds = new DataSet();
-                da.SelectCommand = cmdsql;
-                da.Fill(ds);
-                datagridconsulta.DataSource = ds;
-                datagridconsulta.DataMember = ds.Tables[0].TableName;
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Erro" + ex.Message);
-                throw;
-            }
-            finally
-            {
-                conexao.Close();
-            }
+        private void rdbnome_CheckedChanged(object sender, EventArgs e)
+        {
+            lblcheck.Text = "Digite o nome:";
+            txtnome.Visible = true;
+            dtpDistribuicao.Visible = false;
+        }
+
+        private void rdbdatadistri_CheckedChanged(object sender, EventArgs e)
+        {
+            lblcheck.Text = "Digite a data de Distribuição:";
+            txtnome.Visible = false;
+            dtpDistribuicao.Visible = true;
         }
     }
 }
