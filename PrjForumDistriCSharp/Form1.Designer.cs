32add30cdc0f23307ab7f0795f3763455cfbfe72
Kayan Silva <kayanf.silva@gmail.com>
2016-06-20 00:28:15 -0300

    Faz direito para não perder de novo

--- PrjForumDistriCSharp/Form1.Designer.cs
+++ PrjForumDistriCSharp/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmConsulta));
             this.btnPesquisar = new System.Windows.Forms.Button();
             this.txtnome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -48,10 +49,13 @@
             // 
             // btnPesquisar
             // 
-            this.btnPesquisar.Location = new System.Drawing.Point(502, 53);
+            this.btnPesquisar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPesquisar.BackgroundImage")));
+            this.btnPesquisar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPesquisar.Location = new System.Drawing.Point(485, 43);
             this.btnPesquisar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnPesquisar.Name = "btnPesquisar";
-            this.btnPesquisar.Size = new System.Drawing.Size(87, 28);
+            this.btnPesquisar.Size = new System.Drawing.Size(97, 38);
             this.btnPesquisar.TabIndex = 0;
             this.btnPesquisar.Text = "Pesquisar";
             this.btnPesquisar.UseVisualStyleBackColor = true;
@@ -59,7 +63,8 @@
             // 
             // txtnome
             // 
-            this.txtnome.Location = new System.Drawing.Point(106, 56);
+            this.txtnome.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnome.Location = new System.Drawing.Point(115, 56);
             this.txtnome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtnome.Name = "txtnome";
             this.txtnome.Size = new System.Drawing.Size(364, 22);
@@ -68,27 +73,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.Size = new System.Drawing.Size(106, 16);
             this.label1.TabIndex = 5;
             this.label1.Text = "Pesquise entre:";
             // 
             // lblcheck
             // 
             this.lblcheck.AutoSize = true;
+            this.lblcheck.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblcheck.Location = new System.Drawing.Point(8, 59);
             this.lblcheck.Name = "lblcheck";
-            this.lblcheck.Size = new System.Drawing.Size(92, 16);
+            this.lblcheck.Size = new System.Drawing.Size(101, 16);
             this.lblcheck.TabIndex = 6;
             this.lblcheck.Text = "Digite o nome:";
             // 
             // btnlimpar
             // 
-            this.btnlimpar.Location = new System.Drawing.Point(605, 53);
+            this.btnlimpar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpar.Location = new System.Drawing.Point(604, 43);
             this.btnlimpar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnlimpar.Name = "btnlimpar";
-            this.btnlimpar.Size = new System.Drawing.Size(87, 28);
+            this.btnlimpar.Size = new System.Drawing.Size(94, 38);
             this.btnlimpar.TabIndex = 7;
             this.btnlimpar.Text = "Limpar";
             this.btnlimpar.UseVisualStyleBackColor = true;
@@ -96,6 +104,7 @@
             // 
             // btnsair
             // 
+            this.btnsair.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnsair.Location = new System.Drawing.Point(964, 292);
             this.btnsair.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnsair.Name = "btnsair";
@@ -109,21 +118,21 @@
             // 
             this.datagridconsulta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.datagridconsulta.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.datagridconsulta.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
             this.datagridconsulta.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.datagridconsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.datagridconsulta.Enabled = false;
             this.datagridconsulta.Location = new System.Drawing.Point(12, 99);
             this.datagridconsulta.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.datagridconsulta.Name = "datagridconsulta";
             this.datagridconsulta.RowHeadersVisible = false;
-            this.datagridconsulta.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.datagridconsulta.Size = new System.Drawing.Size(1039, 185);
             this.datagridconsulta.TabIndex = 9;
             // 
             // dtpDistribuicao
             // 
+            this.dtpDistribuicao.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpDistribuicao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpDistribuicao.Location = new System.Drawing.Point(190, 56);
+            this.dtpDistribuicao.Location = new System.Drawing.Point(200, 54);
             this.dtpDistribuicao.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dtpDistribuicao.Name = "dtpDistribuicao";
             this.dtpDistribuicao.Size = new System.Drawing.Size(110, 22);
@@ -132,9 +141,10 @@
             // rdbcivel
             // 
             this.rdbcivel.AutoSize = true;
+            this.rdbcivel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rdbcivel.Location = new System.Drawing.Point(3, 7);
             this.rdbcivel.Name = "rdbcivel";
-            this.rdbcivel.Size = new System.Drawing.Size(53, 20);
+            this.rdbcivel.Size = new System.Drawing.Size(58, 20);
             this.rdbcivel.TabIndex = 11;
             this.rdbcivel.TabStop = true;
             this.rdbcivel.Text = "Cível";
@@ -143,9 +153,10 @@
             // rdbcriminal
             // 
             this.rdbcriminal.AutoSize = true;
+            this.rdbcriminal.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rdbcriminal.Location = new System.Drawing.Point(83, 7);
             this.rdbcriminal.Name = "rdbcriminal";
-            this.rdbcriminal.Size = new System.Drawing.Size(73, 20);
+            this.rdbcriminal.Size = new System.Drawing.Size(80, 20);
             this.rdbcriminal.TabIndex = 12;
             this.rdbcriminal.TabStop = true;
             this.rdbcriminal.Text = "Criminal";
@@ -171,9 +182,10 @@
             // rdbnome
             // 
             this.rdbnome.AutoSize = true;
+            this.rdbnome.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rdbnome.Location = new System.Drawing.Point(296, 7);
             this.rdbnome.Name = "rdbnome";
-            this.rdbnome.Size = new System.Drawing.Size(60, 20);
+            this.rdbnome.Size = new System.Drawing.Size(63, 20);
             this.rdbnome.TabIndex = 15;
             this.rdbnome.TabStop = true;
             this.rdbnome.Text = "Nome";
@@ -183,9 +195,10 @@
             // rdbdatadistri
             // 
             this.rdbdatadistri.AutoSize = true;
+            this.rdbdatadistri.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rdbdatadistri.Location = new System.Drawing.Point(362, 7);
             this.rdbdatadistri.Name = "rdbdatadistri";
-            this.rdbdatadistri.Size = new System.Drawing.Size(143, 20);
+            this.rdbdatadistri.Size = new System.Drawing.Size(154, 20);
             this.rdbdatadistri.TabIndex = 16;
             this.rdbdatadistri.TabStop = true;
             this.rdbdatadistri.Text = "Data de Distribuição";
@@ -196,7 +209,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Beige;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(1077, 331);
             this.Controls.Add(this.rdbdatadistri);
             this.Controls.Add(this.rdbnome);
@@ -212,10 +225,10 @@
             this.Controls.Add(this.btnPesquisar);
             this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "FrmConsulta";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "KaySoft - Consultas do Distribuidor - Guarujá";
             this.Load += new System.EventHandler(this.FrmConsulta_Load);
---
+++ PrjForumDistriCSharp/Form1.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace PrjForumDistriCSharp
+{
+    public partial class FrmConsulta : Form
+    {
+        string selectnobanco;
+
+        public void BancoConsulta()
+        {
+            string strcon = "Server=tcp:kaysoft.database.windows.net,1433;Data Source=kaysoft.database.windows.net;Initial Catalog=DistribuidorGuaruja;Persist Security Info=False;User ID=kayanfds;Password=Kayas190393;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection conexao = new SqlConnection(strcon);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+
+            if(rdbcivel.Checked && rdbnome.Checked)
+            {
+                selectnobanco = "Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where nm_requerido = '" + txtnome.Text + "'";
+            }
+            if (rdbcivel.Checked && rdbdatadistri.Checked)
+            {
+                selectnobanco = "Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where dt_distribuicao = '" + dtpDistribuicao.Value.ToString("yyyyMMdd") + "'";
+            }
+            if(rdbcriminal.Checked && rdbnome.Checked)
+            {
+                selectnobanco = "Select nm_requerido as \"Requerido\", nm_requerente as \"Requerente\", dt_distribuicao as \"Data de Distribuição\", nm_natureza as \"Natureza\", nm_oficio as \"Oficio\", vl_valor as \"Valor\", nm_observacao as \"Observação\" from dbo.FichaCivel where nm_requerido = '" + txtnome.Text + "'";
+            }
+            if(rdbcriminal.Checked && rdbdatadistri.Checked)
+            {
+                selectnobanco = "";
+            }
+
+            SqlCommand cmdsql = new SqlCommand(selectnobanco, conexao);
+            conexao.Open();
+            cmdsql.ExecuteNonQuery();
+            da.SelectCommand = cmdsql;
+            da.Fill(ds);
+            datagridconsulta.DataSource = ds;
+            datagridconsulta.DataMember = ds.Tables[0].TableName;
+            conexao.Close();
+            if (datagridconsulta.RowCount <= 1)
+                MessageBox.Show("Nenhum registro foi encontrado.");
+
+        }
+
+        public FrmConsulta()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmConsulta_Load(object sender, EventArgs e)
+        {
+            rdbcivel.Checked = true;
+            rdbnome.Checked = true;
+            lblcheck.Text = "Digite o nome:";
+            txtnome.Focus();
+            dtpDistribuicao.Visible = false;
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtnome.Clear();
+            datagridconsulta.DataSource = null;
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            BancoConsulta();
+        }
+
+        private void rdbnome_CheckedChanged(object sender, EventArgs e)
+        {
+            lblcheck.Text = "Digite o nome:";
+            txtnome.Visible = true;
+            dtpDistribuicao.Visible = false;
+        }
+
+        private void rdbdatadistri_CheckedChanged(object sender, EventArgs e)
+        {
+            lblcheck.Text = "Digite a data de Distribuição:";
+            txtnome.Visible = false;
+            dtpDistribuicao.Visible = true;
+        }
+    }
+}
